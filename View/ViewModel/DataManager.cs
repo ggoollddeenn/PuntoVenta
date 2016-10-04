70dcf168545b354af54489328e00e7ceb901651c
raulb <raulb@LAPTOP-4UN72CN9>
2016-10-03 19:30:24 -0500

    Creación de clase DataManager

---
+++ View/ViewModel/DataManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+namespace View.ViewModel
+{
+    public class DataManager
+    {
+        public static bool login(string usuario, string contrasena)
+        {
+            bool respuesta = false;
+            using (var Contexto = new BD_VENTAEntities())
+            {
+                var lista = (from empleados in Contexto.EMPLEADO
+                             where empleados.USUARIO == usuario && empleados.CONTRASENA == contrasena
+                             select empleados).ToList();
+                if (lista.Count > 0)
+                {
+                    respuesta = true;
+                }
+            }
+                return respuesta;
+        }
+    }
+}
